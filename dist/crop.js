885301cc24185108b4eb64b889cb7aab9a3ea171
99 <a13589949@gmail.com>
2017-06-13 19:19:54 +0800

    update

--- dist/crop.js
+++ dist/crop.js
@@ -282,34 +282,8 @@ function resetOrientation(srcBase64, srcOrientation, callback, errorCallback) {
       errorCallback ? errorCallback({ code: 1, message: message }) : window.alert(message);
       return;
     }
-    // set proper canvas dimensions before transform & export
-    if ([5, 6, 7, 8].indexOf(srcOrientation) > -1) {
-      canvas.width = height;
-      canvas.height = width;
-    } else {
-      canvas.width = width;
-      canvas.height = height;
-    }
 
-    // transform context before drawing image
-    switch (srcOrientation) {
-      case 2:
-        ctx.transform(-1, 0, 0, 1, width, 0);break;
-      case 3:
-        ctx.transform(-1, 0, 0, -1, width, height);break;
-      case 4:
-        ctx.transform(1, 0, 0, -1, 0, height);break;
-      case 5:
-        ctx.transform(0, 1, 1, 0, 0, 0);break;
-      case 6:
-        ctx.transform(0, 1, -1, 0, height, 0);break;
-      case 7:
-        ctx.transform(0, -1, -1, 0, height, width);break;
-      case 8:
-        ctx.transform(0, -1, 1, 0, 0, width);break;
-      default:
-        ctx.transform(1, 0, 0, 1, 0, 0);
-    }
+    transformCoordinate(canvas, ctx, width, height, srcOrientation);
 
     // draw image
     ctx.drawImage(img, 0, 0);
@@ -319,6 +293,37 @@ function resetOrientation(srcBase64, srcOrientation, callback, errorCallback) {
   };
 
   img.src = srcBase64;
+}
+
+function transformCoordinate(canvas, ctx, width, height, srcOrientation) {
+  // set proper canvas dimensions before transform & export
+  if ([5, 6, 7, 8].indexOf(srcOrientation) > -1) {
+    canvas.width = height;
+    canvas.height = width;
+  } else {
+    canvas.width = width;
+    canvas.height = height;
+  }
+
+  // transform context before drawing image
+  switch (srcOrientation) {
+    case 2:
+      ctx.transform(-1, 0, 0, 1, width, 0);break;
+    case 3:
+      ctx.transform(-1, 0, 0, -1, width, height);break;
+    case 4:
+      ctx.transform(1, 0, 0, -1, 0, height);break;
+    case 5:
+      ctx.transform(0, 1, 1, 0, 0, 0);break;
+    case 6:
+      ctx.transform(0, 1, -1, 0, height, 0);break;
+    case 7:
+      ctx.transform(0, -1, -1, 0, height, width);break;
+    case 8:
+      ctx.transform(0, -1, 1, 0, 0, width);break;
+    default:
+      ctx.transform(1, 0, 0, 1, 0, 0);
+  }
 }
 
 function imgCover(imgW, imgH, divW, divH) {
--- src/util/image.js
+++ src/util/image.js
@@ -52,26 +52,8 @@ export function resetOrientation (srcBase64, srcOrientation, callback, errorCall
       errorCallback ? errorCallback({ code: 1, message }) : window.alert(message)
       return
     }
-    // set proper canvas dimensions before transform & export
-    if ([5, 6, 7, 8].indexOf(srcOrientation) > -1) {
-      canvas.width = height
-      canvas.height = width
-    } else {
-      canvas.width = width
-      canvas.height = height
-    }
 
-    // transform context before drawing image
-    switch (srcOrientation) {
-      case 2: ctx.transform(-1, 0, 0, 1, width, 0); break
-      case 3: ctx.transform(-1, 0, 0, -1, width, height); break
-      case 4: ctx.transform(1, 0, 0, -1, 0, height); break
-      case 5: ctx.transform(0, 1, 1, 0, 0, 0); break
-      case 6: ctx.transform(0, 1, -1, 0, height, 0); break
-      case 7: ctx.transform(0, -1, -1, 0, height, width); break
-      case 8: ctx.transform(0, -1, 1, 0, 0, width); break
-      default: ctx.transform(1, 0, 0, 1, 0, 0)
-    }
+    transformCoordinate(canvas, ctx, width, height, srcOrientation)
 
     // draw image
     ctx.drawImage(img, 0, 0)
@@ -81,6 +63,29 @@ export function resetOrientation (srcBase64, srcOrientation, callback, errorCall
   }
 
   img.src = srcBase64
+}
+
+export function transformCoordinate (canvas, ctx, width, height, srcOrientation) {
+  // set proper canvas dimensions before transform & export
+  if ([5, 6, 7, 8].indexOf(srcOrientation) > -1) {
+    canvas.width = height
+    canvas.height = width
+  } else {
+    canvas.width = width
+    canvas.height = height
+  }
+
+  // transform context before drawing image
+  switch (srcOrientation) {
+    case 2: ctx.transform(-1, 0, 0, 1, width, 0); break
+    case 3: ctx.transform(-1, 0, 0, -1, width, height); break
+    case 4: ctx.transform(1, 0, 0, -1, 0, height); break
+    case 5: ctx.transform(0, 1, 1, 0, 0, 0); break
+    case 6: ctx.transform(0, 1, -1, 0, height, 0); break
+    case 7: ctx.transform(0, -1, -1, 0, height, width); break
+    case 8: ctx.transform(0, -1, 1, 0, 0, width); break
+    default: ctx.transform(1, 0, 0, 1, 0, 0)
+  }
 }
 
 export function imgCover (imgW, imgH, divW, divH) {
