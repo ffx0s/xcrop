bf2eb77d224ab1efc57b95b3af1250a08df1c1d5
99 <a13589949@gmail.com>
2018-02-14 14:40:20 +0800

    添加 banner 配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,18 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import uglify from 'rollup-plugin-uglify'
 import replace from 'rollup-plugin-replace'
-
+const packageJson = require('./package.json')
 const isDev = process.env.NODE_ENV !== 'production'
 
 export default {
   entry: 'src/crop/index.js',
+  banner: `
+  /*!
+   * @name xcrop v${packageJson.version}
+   * @github https://github.com/ffx0s/xcrop
+   * @license MIT.
+   */
+  `,
   dest: isDev ? 'dist/crop.js' : 'dist/crop.min.js',
   format: 'umd',
   moduleName: 'Crop',
@@ -26,6 +33,17 @@ export default {
       exclude: 'node_modules/**',
       ENV: JSON.stringify(process.env.NODE_ENV || 'development')
     }),
-    (!isDev && uglify())
+    (!isDev && uglify({
+      output: {
+        comments (node, comment) {
+          const text = comment.value
+          const type = comment.type
+          if (type === 'comment2') {
+            // multiline comment
+            return /@name|@github|@license/i.test(text)
+          }
+        }
+      }
+    }))
   ]
 }
