3a529916b924c808c6c135a30e27177b98bb446a
ffx0s <a13589949@gamil.com>
2019-05-17 13:39:51 +0800

    修改CSS命名

--- src/crop/template.js
+++ src/crop/template.js
@@ -5,12 +5,12 @@ export default function template (options) {
   } = options
 
   return (
-    '<div class="crop-container" data-el="container">' +
-      '<div class="crop-zoom" data-el="zoom"></div>' +
-      '<div class="crop-mask" data-el="mask"></div>' +
-      '<div class="crop-handle" data-el="handle">' +
-        `<div class="crop-cancle" data-el="cancle" data-click="onCancle">${cancleText}</div>` +
-        `<div class="crop-confirm" data-el="confirm" data-click="onConfirm">${confirmText}</div>` +
+    '<div class="crop" data-el="container">' +
+      '<div class="crop__zoom" data-el="zoom"></div>' +
+      '<div class="crop__mask" data-el="mask"></div>' +
+      '<div class="crop__handle" data-el="handle">' +
+        `<div class="crop__handle-cancle" data-el="cancle" data-click="onCancle">${cancleText}</div>` +
+        `<div class="crop__handle-confirm" data-el="confirm" data-click="onConfirm">${confirmText}</div>` +
       '</div>' +
     '</div>'
   )
